75791a691bacbf9fca8d9a7d604a9f3171c73323
faaman <faaman2020@gmail.com>
2024-07-23 15:32:53 +0530

    changed files

--- counter.js
+++ counter.js
@@ -3,6 +3,7 @@ if (!localStorage.getItem('counter')) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 });
 
