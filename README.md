5341dbc6a75c272ec883a23f1a72716b9e6f36c0
faaman <faaman2020@gmail.com>
2024-08-10 19:24:55 +0530

    more tests

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ This is code from the learning from the CS50 Web Programming, and TAU's Introduc
     - The first section is the Arrange section. The second is Act, and the third is Assert.
 - Ideally, all the variables should come at the very beginning of a file or a method. 
 - A test.describe represents the test case. Inside of describe, you have test scenarios. Inside a test scenario, you can have steps and commands after them, or you can have just a simple test scenario without steps. You could use .only and .skip to run one specific test scenario. All the way back to the top, we do have our variables at the beginning, beforeAll, beforeEach, afterAll, and afterEach.
+- POM is a representation of a page or a group of elements in a page.
+- Unlike toHaveText, toContainText, when specified with the partial text, it will assert and confirm the element with its presence. You don't need to provide the exact text like toHaveText.
+
+## Javascript
+The old versions of JavaScript had no import, include, or require, so many different approaches to this problem have been developed. To enable "ESM" (vs. Node.js's previous CommonJS-style module system you either use "type": "module" in package.json or give the files the extension .mjs. For e.g. const { PlaywrightDevPage } = require('../pages/playwright-dev-page.js') may not work, in that case add "type": "module" in your package.json file.
+ // "type": "module",
--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -23,53 +23,56 @@ test('test some starting link', async ({ page }) => {
     expect(page.getByText('A platform of web services that forms a Bed and Breakfast booking system.')).toBeVisible;
 });
   
-test('check Shady page', async ({ page }) => {
-    await page.goto(URL);
+test.describe.only('bed n breakfast test suite', () => {
 
-    await clickGetStarted(page);
-
-    //this is only useful for a drop-down
-    //await page.getByRole('button', { name: 'Book this room' }).hover();
-
-    await page.getByRole('button', {name: 'Book this room'}).nth(0).click();
-
-    expect(page.getByText('Today')).toBeVisible;
-    expect(page.getByText('Firstname')).toBeVisible;
-    await page.getByLabel('Firstname').fill('Jane');
-    await expect(page.getByText('Lastname')).toBeVisible;
-    await page.getByLabel('Lastname').fill('Austen');
+    test('check Shady page', async ({ page }) => {
+        await page.goto(URL);
     
-    expect(page.getByText('Email')).toBeVisible;
-    //don't use an xpath, this is discouraged because it's not user-visible, and it's fragile, since if a single div changes in the chain, the path will no longer be valid.
-    await page.locator('xpath=/html/body/div/div[2]/div/div[4]/div/div[2]/div[3]/div[3]/input').fill('janea21@newmails.com');
-    await page.getByLabel('Email').nth(0).fill('janea22@newmails.com');
-    await page.getByLabel('Email').nth(1).fill('austenj22@newmails.com'); //this serves no purpose in the flow - just to illustrate the 'nth' function
-
-    expect(page.getByText('Phone')).toBeVisible;
-    //await page.locator('xpath=/html/body/div/div[2]/div/div[4]/div/div[2]/div[3]/div[4]/input').fill('1234567');
-    await page.getByLabel('Phone').nth(0).fill('1234567');
-  
-    await page.getByRole('button', {name: 'Book'}).click();
-  
-    await expect(page.getByText('size must be between 11 and 21'), 'size must be between 11 and 21').toBeVisible();
-    expect(page.getByText('must not be null')).toBeVisible;
-
-    await page.screenshot({ path: 'screenshot_shady_ofexample2.png', fullPage: true });
-
-});
-
-test('check MW page', async ({ page }) => {
-    expect(page.getByText('restful-booker-platform v')).toBeVisible;
-    await page.getByText('Mark Winteringham', { exact: true }).click();
-    await expect(page).toHaveURL('https://www.mwtestconsultancy.co.uk/');
-    await expect(page.getByText('Jane Austen', { exact: true })).not.toBeVisible();
-    await page.getByText('Books', { exact: true }).click();
-    const javaDescription = `I am a published author focusing on topics that span quality, strategy, technical testing and the future of testing. Read on to learn more about my published work.`;
-    await expect(page.getByText(javaDescription)).toBeVisible();
+        await clickGetStarted(page);
     
-    await page.screenshot({ path: 'screenshot_MWpage_ofexample2.png', fullPage: true });
-  
-    // this is for a drop down selection at the top of a page, we don't have it at our AUT:
-    // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
-
-});
+        //this is only useful for a drop-down
+        //await page.getByRole('button', { name: 'Book this room' }).hover();
+    
+        await page.getByRole('button', {name: 'Book this room'}).nth(0).click();
+    
+        expect(page.getByText('Today')).toBeVisible;
+        expect(page.getByText('Firstname')).toBeVisible;
+        await page.getByLabel('Firstname').fill('Jane');
+        await expect(page.getByText('Lastname')).toBeVisible;
+        await page.getByLabel('Lastname').fill('Austen');
+        
+        expect(page.getByText('Email')).toBeVisible;
+        //don't use an xpath, this is discouraged because it's not user-visible, and it's fragile, since if a single div changes in the chain, the path will no longer be valid.
+        await page.locator('xpath=/html/body/div/div[2]/div/div[4]/div/div[2]/div[3]/div[3]/input').fill('janea21@newmails.com');
+        await page.getByLabel('Email').nth(0).fill('janea22@newmails.com');
+        await page.getByLabel('Email').nth(1).fill('austenj22@newmails.com'); //this serves no purpose in the flow - just to illustrate the 'nth' function
+    
+        expect(page.getByText('Phone')).toBeVisible;
+        //await page.locator('xpath=/html/body/div/div[2]/div/div[4]/div/div[2]/div[3]/div[4]/input').fill('1234567');
+        await page.getByLabel('Phone').nth(0).fill('1234567');
+      
+        await page.getByRole('button', {name: 'Book'}).click();
+      
+        await expect(page.getByText('size must be between 11 and 21'), 'size must be between 11 and 21').toBeVisible();
+        expect(page.getByText('must not be null')).toBeVisible;
+    
+        await page.screenshot({ path: 'screenshot_shady_ofexample2.png', fullPage: true });
+    
+    });
+    
+    test('check MW page', async ({ page }) => {
+        expect(page.getByText('restful-booker-platform v')).toBeVisible;
+        await page.getByText('Mark Winteringham', { exact: true }).click();
+        await expect(page).toHaveURL('https://www.mwtestconsultancy.co.uk/');
+        await expect(page.getByText('Jane Austen', { exact: true })).not.toBeVisible();
+        await page.getByText('Books', { exact: true }).click();
+        const javaDescription = `I am a published author focusing on topics that span quality, strategy, technical testing and the future of testing. Read on to learn more about my published work.`;
+        await expect(page.getByText(javaDescription)).toBeVisible();
+        
+        await page.screenshot({ path: 'screenshot_MWpage_ofexample2.png', fullPage: true });
+      
+        // this is for a drop down selection at the top of a page, we don't have it at our AUT:
+        // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
+    
+    });
+})
